00231de9447b46de62aaf76d575e2cd9a3652e14
RoboCodr <129765912+RoboCodr@users.noreply.github.com>
2023-04-07 17:41:41 +0530

    Add files via upload

---
+++ main.py.ai assistant.py
@@ -0,0 +1,24 @@
+name=input("what is your name \n")
+print(name+" how are you")
+reply=input()
+print("how can i help you " + name)
+reply=input()
+if reply=="what is the temprature":
+  print("the temprature is 32 degrees celsius")
+if reply=="what is my phone number":
+  print("your phone number is 1234567890")   
+if reply=="when is my birthday":
+  print("your birthday is on 24th november 2007")
+if reply=="where is my location":
+  print("your location is ABC road,USA") 
+
+
+
+
+else:
+           print("sorry i didnt get you")
+
+  
+
+
+
